e20f780649409b19935ae3f31ee92ea09fe7fe8b
shaafi91 <shaaficiibraahim29@gmail.com>
2025-02-06 08:00:27 -0800

    update

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,28 @@
+const { constants } = require('../config/constants');
+const errorHandler = (err, req, res, next) => {
+const statuscode = res.statusCode ? res.statusCode : 500;
+switch(statuscode){
+    case constants.VALIDATION_ERROR:
+        res.json({ title: "VALIDATION FAILED", message: err.message, stackTrace: err.stack});
+
+    break;
+    case constants.NOT_FOUND:
+        res.json({ title: "NOT FOUND", message: err.message, stackTrace: err.stack});
+    break;
+    case constants.UNAUTHORIZED:
+        res.json({ title: "UNAUTHORIZED", message: err.message, stackTrace: err.stack});
+    break;
+    case constants.FORBIDDEN:
+        res.json({ title: "FORBIDDEN", message: err.message, stackTrace: err.stack});
+    break;
+    case constants.SERVER_ERROR:
+        res.json({ title: "SERVER ERROR", message: err.message, stackTrace: err.stack});
+    default:
+        console.log("no error, all good !");
+        break;
+}
+
+
+};
+
+module.exports = errorHandler;
